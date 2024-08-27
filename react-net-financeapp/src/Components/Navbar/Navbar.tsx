13f0ca77dc51a3d39401d3ecf7403df3e946ec16
Mike Velasco <mvelasco2020@outlook.com>
2024-08-26 21:53:16 -0500

    hamburger navbar

--- react-net-financeapp/src/Components/Navbar/Navbar.tsx
+++ react-net-financeapp/src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 interface Props {}
 
 const Navbar = (props: Props) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
@@ -24,7 +30,39 @@ const Navbar = (props: Props) => {
             Signup
           </a>
         </div>
+        <div className="lg:hidden">
+          <button onClick={toggleMenu} className="text-darkBlue focus:outline-none">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
+      {isMenuOpen && (
+        <div className="lg:hidden mt-4 space-y-4">
+          <a href="" className="block text-black hover:text-darkBlue">
+            Dashboard
+          </a>
+          <div className="hover:text-darkBlue">Login</div>
+          <a
+            href=""
+            className="block px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+          >
+            Signup
+          </a>
+        </div>
+      )}
     </nav>
   );
 };
